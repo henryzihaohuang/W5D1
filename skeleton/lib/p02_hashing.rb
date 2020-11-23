1e1290a61b52df062303f72a65e73154c227160b
David Suh <davidisuh93@gmail.com>
2020-11-23 14:53:50 -0500

    hashing third commit

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -30,9 +30,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    # self.to_a.each do |subarray|
-    #   subarray.hash
-    # end
-    0
+    flattened = self.to_a.flatten
+    flattened.length.each do |i|
+    end
   end
 end
