972d99b6890cb4601af059d02d44b2ec66d2cb14
David Suh <davidisuh93@gmail.com>
2020-11-23 14:09:23 -0500

    p02 hashing first commit

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,18 @@ end
 
 class Array
   def hash
+    self.each do |ele|
+      ele.hash
+    end
   end
 end
 
 class String
   def hash
+    chars = self.chars
+    chars.each do |char|
+      char.hash
+    end
   end
 end
 
@@ -16,6 +23,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.each do |ele|
+      ele.hash
+    end
   end
 end
